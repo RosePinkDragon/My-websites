83c2b84d5b0ea862984b9ac3c181e8f260d08cfe
RosePinkDragon <59683036+RosePinkDragon@users.noreply.github.com>
2020-09-07 14:54:28 +0530

    done till second last tut

--- JavaScript/functionsAndMethods.js
+++ JavaScript/functionsAndMethods.js
@@ -9,3 +9,40 @@ console.log(resultOne);
 //methods
 //are funtions that are defined for an object and data types
 console.log(name.toUpperCase());
+
+//calbacks and foreach
+//callbacks are used for for each
+
+const myFunc = (callbcakFunc) => {
+    let value = 50;
+    callbcakFunc(value);
+};
+
+myFunc(value => {
+    console.log(value);
+});
+
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+people.forEach ((people, index) => {
+    console.log('i is', people, index);
+});
+
+const logPerson = (person, index) => {
+    console.log(`${index+1} - hello ${person}`);
+};
+
+people.forEach(logPerson);
+
+//refrence the ul tag in html page
+const ul = document.querySelector('.people');
+
+let html = ``;
+
+people.forEach(person => {
+    html += `<li style="color: purple">${person}</li>`;
+});
+
+console.log(html);
+
+ul.innerHTML = html;
