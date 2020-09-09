402c7325f00b3c6a3114e21f1185fe89e2a2869d
RosePinkDragon <59683036+RosePinkDragon@users.noreply.github.com>
2020-09-09 22:44:29 +0530

    node till 2 lec

---
+++ Nodejs/public/global.js
@@ -0,0 +1,19 @@
+//global object
+
+// setTimeout(() => {
+//     console.log('Timeout');
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log('.');
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(document.querySelector);
+
+const os = require('os')
+
+console.log(os.platform(), os.homedir)
